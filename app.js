1660ed5cdb4a853e483c0eb6286eff71f76cce40
mariosky <mariosky@gmail.com>
2016-11-10 11:00:06 -0800

    public folder

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(require('express-session')({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'))
 
 app.use('/', routes);
 app.use('/users', users);
